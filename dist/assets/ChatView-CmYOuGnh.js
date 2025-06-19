63534b668799da4b10333d4456e9bb257e92ecfe
bakafox <v.chekhovskii@g.nsu.ru>
2025-06-19 20:58:57 +0700

    э шэйм зэн ай кэн риииид йоо маааайнд

--- dist/assets/ChatView-CmYOuGnh.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,c,i as n}from"./index-Cvh_soG3.js";const t={};function r(a,o){return n(),c("main",null," CHAT VIEW ")}const _=e(t,[["render",r]]);export{_ as default};
---
+++ dist/assets/SettingsView-BybWtsxZ.js
@@ -0,0 +1,1 @@
+import{_ as o,p as t,h as n,i as c}from"./index-l4TE-YJ1.js";const r={};function s(_,a){const e=n("RouterView");return c(),t(e)}const p=o(r,[["render",s]]);export{p as default};
