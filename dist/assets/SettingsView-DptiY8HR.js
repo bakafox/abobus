1115bd4a90d0acdeff017721dc2dd2a34abfd179
bakafox <v.chekhovskii@g.nsu.ru>
2025-06-20 18:38:48 +0700

    кендэл лит смайл дэт вииии бооф щееееее

--- dist/assets/SettingsView-DptiY8HR.js
+++
@@ -1,1 +0,0 @@
-import{_ as o,m as t,x as n,o as c}from"./index-CUoSi8b9.js";const r={};function s(_,a){const e=n("RouterView");return c(),t(e)}const f=o(r,[["render",s]]);export{f as default};
---
+++ dist/assets/SettingsView-UyKtS1HA.js
@@ -0,0 +1,1 @@
+import{_ as o,m as t,x as n,o as c}from"./index-B7-GSVGD.js";const r={};function s(_,a){const e=n("RouterView");return c(),t(e)}const f=o(r,[["render",s]]);export{f as default};
